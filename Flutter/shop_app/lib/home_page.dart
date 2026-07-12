030884eedf691e2faa801ed3df3e2ce292e3aa22
sunjirsanuj <sunjirislam2019@gmail.com>
2026-07-12 14:58:09 +0600

    half way

--- Flutter/shop_app/lib/home_page.dart
+++ Flutter/shop_app/lib/home_page.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/global_variables.dart';
+import 'package:shop_app/product_card.dart';
+import 'package:shop_app/product_details_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const ["All", "Adidas", "Nike", "Bata"];
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedFilter = filters[0];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +26,7 @@ class HomePage extends StatelessWidget {
       borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
       borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
     );
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
@@ -19,7 +36,7 @@ class HomePage extends StatelessWidget {
                   padding: const EdgeInsets.all(20.0),
                   child: Text(
                     "Shoes\nCollection",
-                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
                 ),
                 Expanded(
@@ -34,6 +51,72 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                itemCount: filters.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  final filter = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedFilter = filter;
+                        });
+                      },
+                      child: Chip(
+                        label: Text(filter),
+                        labelStyle: TextStyle(fontSize: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(25),
+                        ),
+                        backgroundColor: selectedFilter == filter
+                            ? Theme.of(context).colorScheme.primary
+                            : Color.fromRGBO(245, 247, 249, 1),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 15,
+                        ),
+                        side: BorderSide(
+                          color: Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  final product = products[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return ProductDetailsPage(product: product);
+                          },
+                        ),
+                      );
+                    },
+                    child: ProductCard(
+                      title: product["title"] as String,
+                      price: product["price"] as double,
+                      image: product["imagesUrl"] as String,
+                      backgroundColor: index.isEven
+                          ? Color.fromRGBO(216, 240, 253, 1)
+                          : Color.fromRGBO(245, 247, 249, 1),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
--- Flutter/shop_app/lib/main.dart
+++ Flutter/shop_app/lib/main.dart
@@ -16,13 +16,43 @@ class MyApp extends StatelessWidget {
 
       theme: ThemeData(
         fontFamily: "Lato",
+
         colorScheme: ColorScheme.fromSeed(
           seedColor: Color.fromRGBO(254, 206, 1, 1),
+          primary: Color.fromRGBO(254, 206, 1, 1),
         ),
+
         inputDecorationTheme: InputDecorationTheme(
-          hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16,color: Color.fromRGBO(119, 119, 119, 1)),
+          hintStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+            color: Color.fromRGBO(119, 119, 119, 1),
+          ),
           prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
         ),
+
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 35,
+          ),
+
+          titleMedium: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+
+          bodySmall: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          )
+        ),
+
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        useMaterial3: true
       ),
 
       home: HomePage(),
